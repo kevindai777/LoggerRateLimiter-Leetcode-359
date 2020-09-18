4b3e5b5355ab5ce4f86a01c3396d285f7349692a
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-18 17:57:10 -0400

    added java solution

---
+++ loggerratelimiter.java
@@ -0,0 +1,22 @@
+//Java Solution
+
+class Logger {
+
+    /** Initialize your data structure here. */
+    private Map<String, Integer> map;
+    public Logger() {
+        map = new HashMap<>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if (map.containsKey(message) && timestamp - map.get(message) < 10) {
+            return false;
+        }
+        
+        map.put(message, timestamp);
+        return true;
+    }
+}
